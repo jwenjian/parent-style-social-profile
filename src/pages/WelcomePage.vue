6c725ebfa8de52ae985e74fd523248124685a82e
Jiang Wenjian <wenjian.jiang@foxmail.com>
2020-08-01 14:37:21 +0800

    update welcome tips

--- src/pages/WelcomePage.vue
+++ src/pages/WelcomePage.vue
@@ -6,7 +6,8 @@
     <div ref="ref-0">
       <h3>Parent style(家长风格)微信昵称集散地</h3>
     </div>
-    <div ref="ref-2"><el-button :type="btnType" @click="nextAnimation">开始探索 -> </el-button>
+    <div ref="ref-2">
+      <el-button :type="btnType" @click="nextAnimation">开始探索 -></el-button>
     </div>
     <div class="hidden tips" ref="ref-3">他（她）们也曾年轻过</div>
     <div class="hidden tips" ref="ref-4">热血过</div>
@@ -14,7 +15,7 @@
     <div class="hidden tips" ref="ref-6">只是后来时光慢慢磨平了岁月的棱角</div>
     <div class="hidden tips" ref="ref-7">琐碎的生活点滴成为了他们生活的全部</div>
     <div class="hidden tips" ref="ref-8">“相识是缘”，“平安一生”</div>
-    <div class="hidden tips" ref="ref-9" >这些看似 [俗气] 的微信昵称</div>
+    <div class="hidden tips" ref="ref-9">这些看似 [俗气] 的微信昵称</div>
     <div class="hidden tips" ref="ref-10">却正是他们饱经岁月洗礼的最好证据</div>
     <div class="hidden tips" ref="ref-11">现在，再点击一次</div>
     <div class="hidden tips" ref="ref-12">探索属于你的家长风昵称, Go!</div>
@@ -25,32 +26,47 @@
 const max_ref = 12;
 
 export default {
-  name: 'welcome-page',
+  name: "welcome-page",
   data() {
     return {
-      btnType: 'info',
-      ref: 2
-    }
+      btnType: "info",
+      ref: 2,
+      itr: null,
+    };
   },
   methods: {
     nextAnimation() {
       if (this.ref >= max_ref) {
-        this.$router.push('/explore')
+        this.$router.push("/explore");
         return;
       }
-      this.ref = this.ref + 1;
-      this.$refs[`ref-${this.ref}`].classList.add('show', 'animate__animated', 'animate__backInUp')
-      if (this.ref == max_ref) {
-        this.btnType = 'success';
-      } else {
-        this.btnType = 'info'
-      }
-    }
+      this.itr = setInterval(() => {
+        if (this.ref == max_ref) {
+          clearInterval(this.itr)
+          return
+        }
+        this.ref = this.ref + 1;
+        this.$refs[`ref-${this.ref}`].classList.add(
+          "show",
+          "animate__animated",
+          "animate__backInUp"
+        );
+        if (this.ref == max_ref) {
+          this.btnType = "success";
+        } else {
+          this.btnType = "info";
+        }
+      }, 500);
+    },
   },
   mounted() {
-    this.$refs['ref-2'].classList.add('animate__animated', 'animate__tada', 'animate__infinite')
-  }
-}
+    this.$refs["ref-2"].classList.add(
+      "animate__animated",
+      "animate__tada",
+      "animate__infinite"
+    );
+  },
+};
 </script>
 
 <style>
