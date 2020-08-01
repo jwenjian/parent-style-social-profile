612fdc32c12ab347fb6aa17657c09ba5917738a0
Jiang Wenjian <wenjian.jiang@foxmail.com>
2020-08-01 12:30:13 +0800

    banner and avatar ok

--- src/pages/ExplorePage.vue
+++ src/pages/ExplorePage.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>Explore</div>
+  <div class="flex-wrapper">
+    <div class="banner">
+    </div>
+    <div class="avatar">
+      <img src="/rose.jpeg" alt="" srcset="">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -12,5 +18,26 @@ export default {
 </script>
 
 <style>
+.flex-wrapper {
+width: min(100vh, 500px);
+height: 100vh;
+margin: auto auto;
+background-color: aliceblue;
+}
+
+.banner {
+  background-image: url('/banner.jpeg');
+  height: 40vh;
+  background-size: cover;
+}
+
+.avatar > img {
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+  float: right;
+  margin-top: -70px;
+  margin-right: 10px;
+}
 
 </style>
